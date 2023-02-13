8ddd0c68b275e24852c1a5981b656ac51d8ca304
José Arévalo <joseaf1002@gmail.com>
2023-02-13 15:25:46 -0600

    Add response function

---
+++ network/response.js
@@ -0,0 +1,13 @@
+function success(req, res, msg, status) {
+  res.status(status || 200).json({
+    error: "",
+    body: msg
+  });
+}
+
+function error(req, res, msg, status) {
+  res.status(status || 500).json({
+    error: msg,
+    body: ""
+  });
+}
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const routerApi = require('./routes/index.js');
+
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+
+routerApi(app);
 
 app.listen(port, () => {
   console.log(`Server corriendo en: http:77localhost:${port}` );
